4ea5227b100441190447f1fa57bcdcc71048662d
Antsatianafifa <antsatianafifarakotoniaina@gmail.com>
2023-04-06 10:34:53 +0300

    first commit

---
+++ HTML/myScript.js
@@ -0,0 +1,5 @@
+var projet= document.getElementById('projet');
+var repetition= document.getElementById('repetition');
+var duree= document.getElementsByClassName('duree');
+
+projet.addEventListener
